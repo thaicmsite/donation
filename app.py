9c2061a398d18c53ce1c17f920a546eb3a4dc286
thaicmsite <thaicmsite@gmail.com>
2022-12-05 17:15:01 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 from flask import Flask
-from rembg import remove
-from PIL import Image
-Image.LOAD_TRUNCATED_IMAGES = True
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    input_path = 'pro-golf.jpg'
-    output_path = 'output-pro-golf.png'
-    input = Image.open(input_path)
-    output = remove(input)
-    output.save(output_path)
-
     return "Hello World"
